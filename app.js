09e8c1afa2418da910d9ab9ccf7f5d0943c0e52e
kiburtaite <kiburtaite@gmail.com>
2022-06-08 03:49:09 +0300

    login route plus

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import register from './routes/api/register.js';
-import { engine } from 'express-handlebars'
+import login from './routes/api/login.js';
+import { engine } from 'express-handlebars';
+import cookieParser from 'cookie-parser';
 import register_page from './routes/ui/register_page.js';
 
 const app = express();
@@ -19,12 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: false
 }));
+app.use(cookieParser());
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.use('/register', register_page);
+app.use('/api/login', login);
 app.use('/api/register', register);
 
 app.listen(PORT, () => console.log(`Serveris veikia ant ${PORT} porto`))
