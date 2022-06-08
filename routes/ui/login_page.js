620328f66d5556c858b3861fae6415adb7be6214
kiburtaite <kiburtaite@gmail.com>
2022-06-08 04:16:49 +0300

    login page

---
+++ routes/ui/login_page.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.render('login', {
+        title: 'Prisijungimas'
+    })
+});
+
+export default router
