88aa4b5680611b8acb50434eda99374c86cef12c
kiburtaite <kiburtaite@gmail.com>
2022-06-09 04:30:29 +0300

    bills page auth

--- routes/ui/bills_page.js
+++ routes/ui/bills_page.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import con from '../../connection.js';
+import authorization from '../../authorization.js';
 
 const router = express.Router();
 
-router.get('/:group_id', async (req,res) => {
+router.get('/:group_id', authorization, async (req,res) => {
     const [data] = await con.query(
         `SELECT * FROM test_db.bills
         WHERE test_db.bills.group_id_bills = ?`, 
