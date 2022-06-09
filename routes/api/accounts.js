b5160b436f87101a689d849afb0a6bbc41726fb2
kiburtaite <kiburtaite@gmail.com>
2022-06-09 05:08:30 +0300

    accounts auth

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,28 +1,29 @@
 import express from 'express';
 import con from '../../connection.js';
+import authorization from '../../authorization.js';
 
 const router = express.Router();
 
-router.get('/:id', async (req, res) => {
+router.get('/', authorization, async (req, res) => {
     try{
         const [data] = await con.query(`
         SELECT * FROM test_db.accounts
         LEFT OUTER JOIN test_db.groups
         ON test_db.accounts.group_id_accounts=test_db.groups.id
         WHERE test_db.accounts.user_id = ?`
-        [req.params.id]);
+        [req.user_id]);
         res.send(data);
     } catch (err){
         res.status(400).send({err})
     }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', authorization, async (req, res) => {
     try{
         const [data] = await con.query(
         `INSERT INTO test_db.accounts SET ?`, {
             group_id_accounts: req.query.group_id,
-            user_id: ''
+            user_id: req.user_id
         });
         res.send(data);
     } catch (err){
